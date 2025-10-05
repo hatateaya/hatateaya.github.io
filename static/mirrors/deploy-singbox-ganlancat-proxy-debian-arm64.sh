eb5397e69f42c5d7706b8e94137d4d40119c5866
hatateaya <amcjedhfeh@gmail.com>
2025-10-05 09:17:25 +0200

    fix sth in sub, upload singbox deb, add a script

---
+++ static/mirrors/deploy-singbox-ganlancat-proxy-debian-arm64.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade
+mkdir tmp-singbox-inst
+cd tmp-singbox-inst
+wget http://ganlanc.at/mirrors/sing-box_1.12.8_linux_amd64.deb
+sudo dpkg -i sing-box_1.12.8_linux_amd64.deb
+wget http://ganlanc.at/subscribe
+sudo mv /etc/sing-box/config.json /etc/sing-box/config.json.bak
+sudo mv ./subscribe /etc/sing-box/config.json
+sudo systemctl enable sing-box --now
