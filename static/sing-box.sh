cf1e1461702c4734f5851f5410f2f746cf3ed33c
hatateaya <amcjedhfeh@gmail.com>
2025-11-30 07:32:22 +0100

    fix typo in sh script

--- static/sing-box.sh
+++ static/sing-box.sh
@@ -10,11 +10,11 @@ download() {
         exit 1
     fi
 }
-if [[ ! -f sing-box ]]; then
+if [ ! -f sing-box ]; then
 	download "https://ganlanc.at/mirrors/linux/sing-box" "sing-box"
 	chmod +x sing-box
 fi
-if [[ ! -f subscribe ]]; then
+if [ ! -f subscribe ]; then
 	download "https://ganlanc.at/subscribe" "subscribe"
 fi
 sudo ip link delete tun0 2>/dev/null || true
