4e48677eb5cea1b575da2956833aa193b440b5d1
hatateaya <amcjedhfeh@gmail.com>
2026-03-22 07:19:53 +0100

    modified mirors and sing-box.sh

--- static/sing-box.sh
+++ static/sing-box.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 download() {
     local url="$1"
     local output="$2"
@@ -10,13 +11,18 @@ download() {
         exit 1
     fi
 }
+
+BASE_URL="http://[2a14:7c0:1002:106d::]"
+
 if [ ! -f sing-box ]; then
-	download "https://ganlanc.at/mirrors/linux/sing-box" "sing-box"
-	chmod +x sing-box
+    download "${BASE_URL}/mirrors/linux/sing-box" "sing-box"
+    chmod +x sing-box
 fi
-if [ ! -f subscribe ]; then
-	download "https://ganlanc.at/subscribe" "subscribe"
+
+if [ ! -f config.json ]; then
+    download "${BASE_URL}/config.json" "config.json"
 fi
+
 sudo ip link delete tun0 2>/dev/null || true
-sudo ./sing-box run -c subscribe
+sudo ./sing-box run -c config.json
 read
